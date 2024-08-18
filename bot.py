d8b235062be56ac249608e38348dcf94c7444571
naviafidela <naviafidela@gmail.com>
2024-08-18 11:40:03 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 # bot.py
 import random
 import asyncio
-from aiogram import Bot, types
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.utils import markdown as md
-from aiogram import Application
 from asupanmu_vip import msg_asupanmu_vip
 from kontol_monster import msg_kontol_monster
 from channel import CHANNELS
@@ -12,7 +11,7 @@ from channel import CHANNELS
 API_TOKEN = '7520123514:AAG8JcQ6H0zjV_eTtK2V84jR0J1cf9wq6lg'
 
 bot = Bot(token=API_TOKEN)
-app = Application()
+dp = Dispatcher(bot)
 
 async def send_random_message():
     while True:
@@ -29,13 +28,13 @@ async def start_handler(message: types.Message):
     await message.answer("Bot ini aktif dan mengirim pesan setiap menit!")
 
 # Daftarkan handler
-app.add_handler(Command("start")(start_handler))
+dp.message_handlers.register(start_handler, Command("start"))
 
 async def main():
     # Start the message sending task
     asyncio.create_task(send_random_message())
     # Start polling
-    await app.run_polling()
+    await dp.start_polling()
 
 if __name__ == '__main__':
     asyncio.run(main())
