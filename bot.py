a885a0e7d6da9d1982f552a76168ac13efcf31cb
naviafidela <naviafidela@gmail.com>
2024-08-18 11:41:35 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 # bot.py
 import random
 import asyncio
-from aiogram import Bot, types
-from aiogram.filters import Command
-from aiogram.utils import markdown as md
-from aiogram import Application, Dispatcher
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from aiogram.types import ParseMode
 from asupanmu_vip import msg_asupanmu_vip
 from kontol_monster import msg_kontol_monster
 from channel import CHANNELS
@@ -12,7 +11,7 @@ from channel import CHANNELS
 API_TOKEN = '7520123514:AAG8JcQ6H0zjV_eTtK2V84jR0J1cf9wq6lg'
 
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(bot)
 
 async def send_random_message():
     while True:
@@ -20,21 +19,17 @@ async def send_random_message():
         message_text = "\n".join(messages)
         for channel in CHANNELS:
             try:
-                await bot.send_message(chat_id=channel, text=message_text, parse_mode=md.ParseMode.HTML)
+                await bot.send_message(chat_id=channel, text=message_text, parse_mode=ParseMode.HTML)
             except Exception as e:
                 print(f"Failed to send message to {channel}: {e}")
         await asyncio.sleep(60)  # Tunggu 1 menit
 
+@dp.message_handler(commands=['start'])
 async def start_handler(message: types.Message):
-    await message.answer("Bot ini aktif dan mengirim pesan setiap menit!")
+    await message.reply("Bot ini aktif dan mengirim pesan setiap menit!")
 
-async def main():
-    app = Application()
-    app.add_handler(Command("start")(start_handler))
-    # Start the message sending task
+async def on_startup(dp):
     asyncio.create_task(send_random_message())
-    # Start polling
-    await app.run_polling()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    executor.start_polling(dp, on_startup=on_startup)
