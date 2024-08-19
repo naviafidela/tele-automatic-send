f2f340161747c0890548b46af31752a478ca8b56
naviafidela <naviafidela@gmail.com>
2024-08-19 20:53:42 +0700

    Update channel.py

--- channel.py
+++ channel.py
@@ -2,5 +2,48 @@
 CHANNELS = [
     '@videy_viral',
     '@videy_bokep_indo',
-    # Tambahkan ID channel lain di sini
+    
+    #01
+    '@ViralDoodstreamIndo_xx',
+    '@VideoDoodstreamTerbaru_xx',
+    '@DoodstreamBokepIndo_xx',
+    '@DoodstreamIndonesiaViral_xx',
+    '@DoodstreamViralTerbaru2024_xx',
+    '@DoodstreamViralTerbaru_xx',
+
+    #02
+    '@TeraboxYangLagiViral',
+    '@TeraboxVideoViral2024',
+    '@TeraboxBokepIndo2024',
+    '@TeraboxBokepTerbaru_xx',
+    '@TeraboxUpdateIndo',
+    '@TeraboxBokepIndonesia_xx',
+    '@TeraboxViralTerbaru_xx',
+
+    #03
+    '@VideyIndonesia',
+    '@VideyTiktokIndo',
+    '@VideyBokepIndoTerbaru',
+    '@VideyTiktokViral',
+    '@VideyViral2024',
+    '@VideyBokepIndo',
+    '@VideyViralTerbaru',
+
+    #04
+    '@BokepIndoTemanKuliah',
+    '@BokepIndoAyamKampus_xx',
+    '@BokepIndoKeciduk',
+    '@BokepIndoIbuKos',
+    '@BokepIndoSMPColmek',
+    '@BokepIndoSMPSempit',
+    '@BokepIndoKetahuanWarga',
+
+    #05
+    '@BokepIndoSMPTerbaru_xx',
+    '@BokepHijab_xx',
+    '@BokepMsbreewc_xx',
+    '@BokepErikaViral_xx',
+    '@BokepViralDewasaVideo_xx',
+    '@AudreyDavisViralVideo_xx',
+    '@BokepViralAudrey_xx',
 ]
