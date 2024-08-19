72c3cbb151f027a3e1396946809378c1f55065ee
naviafidela <naviafidela@gmail.com>
2024-08-19 20:55:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import random
 import asyncio
 import sys
 import time
+import aiohttp
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from aiogram.types import ParseMode
@@ -12,8 +13,9 @@ from channel import CHANNELS
 
 API_TOKEN = '7520123514:AAG8JcQ6H0zjV_eTtK2V84jR0J1cf9wq6lg'
 
-# Menambahkan request_timeout untuk memperpanjang waktu tunggu
-bot = Bot(token=API_TOKEN, request_timeout=60)
+# Membuat sesi aiohttp dengan request_timeout
+session = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=60))
+bot = Bot(token=API_TOKEN, session=session)
 dp = Dispatcher(bot)
 
 async def send_random_message():
@@ -54,5 +56,8 @@ async def start_handler(message: types.Message):
 async def on_startup(dp):
     asyncio.create_task(send_random_message())
 
+async def on_shutdown(dp):
+    await session.close()
+
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
