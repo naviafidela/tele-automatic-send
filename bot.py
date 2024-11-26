27e8af6c7916e81f6731b3103ca92aa7e47abf93
naviafidela <naviafidela@gmail.com>
2024-11-26 20:33:16 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import time
 import sys
 from telegram import Bot
 from telegram.constants import ParseMode
-from telegram.ext import Application, CommandHandler, JobQueue
+from telegram.ext import Application, CommandHandler, CallbackContext
 from message import message
 from channel import CHANNELS
 
@@ -12,7 +12,7 @@ API_TOKEN = '7508753099:AAEDEAogPWH2Z13TmfJn0efWKImPLTI-7h8'
 bot = Bot(token=API_TOKEN)
 application = Application.builder().token(API_TOKEN).build()
 
-async def send_random_message(context):
+async def send_random_message(context: CallbackContext):
     while True:
         all_messages = message
 
@@ -52,6 +52,5 @@ application.add_handler(start_handler)
 
 if __name__ == '__main__':
     # Menambahkan JobQueue untuk menjalankan pengiriman pesan random
-    job_queue = application.job_queue
-    job_queue.run_once(lambda context: send_random_message(context), 0)  # Menjadwalkan pekerjaan
+    application.job_queue.run_once(lambda context: send_random_message(context), 0)  # Menjadwalkan pekerjaan
     application.run_polling()
